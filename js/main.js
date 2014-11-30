73bc52ff000137894077646e7d1172bb6cb57835
basilesimon <basile@basilesimon.fr>
2014-11-30 10:38:02 +0100

    Model JS - just for templating purpose

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function buildPlaylist(json) {
+  $.getJSON(json, function(data) {
+    for (var i=0; i<data.tracks.length; i++) {
+      var tracksListDiv = document.getElementById("tracks-list");
+      var tracks = data.tracks;
+      $("#tracks-list").append(
+      $("<li>").append(
+          $("<a>").attr("href","#").append(
+              $("<div>").attr("class", "track-title").append(tracks[i].title)
+      )));
+     }
+  });
+};
 
+buildPlaylist("playlist-model.json");
