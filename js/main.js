8d3c72abfba63afad107e894295fcefec93c060f
basilesimon <basile@basilesimon.fr>
2014-11-30 10:57:10 +0100

    Model JS now ingests JSON properly and works.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ function buildPlaylist(json) {
     for (var i=0; i<data.tracks.length; i++) {
       var tracksListDiv = document.getElementById("tracks-list");
       var tracks = data.tracks;
+      var title = tracks[i].title;
+      var artist = tracks[i].artist;
+      var type = tracks[i].type;
+      var duration = tracks[i].duration;
+
       $("#tracks-list").append(
-      $("<li>").append(
-          $("<a>").attr("href","#").append(
-              $("<div>").attr("class", "track-title").append(tracks[i].title)
-      )));
+        "<li><a href='#'><div class='track-title'>" + title + "</div><br>" + "<small class='track-author'>" + artist + " -  " + type + " -  " + duration + "</small></a></li>");
      }
   });
 };
