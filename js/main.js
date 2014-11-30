4a1cdfbe560f9a94ce5d056b3dfc72032340dece
basilesimon <basile@basilesimon.fr>
2014-11-30 13:31:59 +0100

    Now playing element + progress bar. :notes:

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ function buildPlaylist(json) {
 };
 
 buildPlaylist("playlist-model.json");
+
+$(document).ready(function() {
+  var progressbar = $('#progressbar'),
+      max = progressbar.attr('max'),
+      time = (1000 / max) * 5,
+      value = progressbar.val();
+
+  var loading = function() {
+    value += 1;
+    addValue = progressbar.val(value);
+
+    $('.progress-value').html(value + '%');
+      if (value == max) {
+        clearInterval(animate);
+      }
+  };
+
+  var animate = setInterval(function() {
+    loading();
+  }, time);
+});
